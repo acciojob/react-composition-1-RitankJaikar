23e6043c2b79d04bbefe5e0e5e0c24a52524d2dc
RitankJaikar <ritank.jaikar@gmail.com>
2024-12-25 09:48:18 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [value, setValue] = useState("Tab 1");
+  const [arr, setArr] = useState(["Tab 1", "Tab 2", "Tab 3"]);
+
+  function handleClick(item1) {
+    setValue(arr.find(item2 => item2 === item1))
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <ul>
+          {
+            arr.map((item, i) => (
+              <li key={i+1} onClick={() => handleClick(item)}>
+                {item}
+              </li>
+            ))
+          }
+        </ul>
+        <div>This is content for {value}</div>
     </div>
   )
 }
